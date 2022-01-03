9799e1c18ae949c321fac8300dd647c47b83c2ab
Siddhantarora2k <91028094+Siddhantarora2k@users.noreply.github.com>
2022-01-03 13:54:31 -0500

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -18,12 +18,12 @@ CREATE TABLE employees (
 
 CREATE TABLE dept_manager (
 dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	primary key (emp_no, dept_no)
+    PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE salaries (
@@ -36,13 +36,13 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-  dept_no VARCHAR NOT NULL,
-  emp_no INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (dept_no)
+ emp_no INT NOT NULL,
+ dept_no VARCHAR NOT NULL,
+ from_date DATE NOT NULL,
+ to_date DATE NOT NULL,
+ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+ PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -50,10 +50,9 @@ CREATE TABLE titles (
  title VARCHAR NOT NULL,
  from_date DATE NOT NULL,
  to_date DATE NOT NULL,
- FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
- PRIMARY KEY (emp_no)
+ FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 SELECT * 
-FROM departments;
+FROM titles;
 
